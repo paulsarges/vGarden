b7a2bdd6d84660bbc3366d42a55b616eda73d7fe
pauls <sargespaul@gmail.com>
2022-02-02 17:22:35 +0100

    +Class Emplacement / -Class Chat

---
+++ src/main/java/vgarden/model/Emplacement.java
@@ -0,0 +1,35 @@
+package vgarden.model;
+
+public class Emplacement {
+	
+	private double positionX;
+	private double positionY;
+	
+		
+	public Emplacement() {
+		
+	}
+
+	public Emplacement(double positionX, double positionY) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+
+	public double getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(double positionX) {
+		this.positionX = positionX;
+	}
+
+	public double getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(double positionY) {
+		this.positionY = positionY;
+	}
+	
+	
+}
