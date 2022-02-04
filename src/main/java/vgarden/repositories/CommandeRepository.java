c52a179fcc4e4774141c10c542122991f5e26b46
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-02-05 00:15:38 +0100

    Using package repositories instead of repository

--- src/main/java/vgarden/repositories/CommandeRepository.java
+++ src/main/java/vgarden/repositories/CommandeRepository.java
@@ -1,11 +1,10 @@
-package vgarden.repository;
+package vgarden.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vgarden.model.Commande;
 
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
-    // @Query("SELECT c from Commande c join fetch c.commandeProduits where c.id=:id")
     @EntityGraph(value = "Commande.CommandeProduits", type = EntityGraph.EntityGraphType.FETCH)
     Commande findByIdAndFetchCommandeProduits(Long id);
 }
