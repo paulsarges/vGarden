3cff17b9df53e279cb0d1878f1899451cff2de08
Fitiantsoa <afitiantsoa@gmail.com>
2022-02-04 17:47:05 +0100

    ajout repository pour Compte

--- src/test/java/vgarden/CompteRepositoryTest.java
+++ src/test/java/vgarden/CompteRepositoryTest.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -21,12 +22,13 @@ public class CompteRepositoryTest {
 	
 	@Test
 	@Transactional
+	@Commit
 	void test() {
 		Admin admin = new Admin();
-		System.out.println(admin);
 		admin.setLogin("test");
 		admin.setPassword("test");
-		
+		System.out.println("--------------------");
+		System.out.println(admin);
 		amdinRepo.save(admin);
 	}
 }
