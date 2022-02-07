8432653fa5c243accb4e0194fb2a14d090536692
IanCretaud <ian.cretaud@gmail.com>
2022-02-07 15:08:29 +0100

    Plante corrrection + repo + début repoTest

---
+++ src/main/java/vgarden/exceptions/PlanteException.java
@@ -0,0 +1,12 @@
+package vgarden.exceptions;
+
+public class PlanteException extends RuntimeException {
+
+    public PlanteException() {
+
+    }
+
+    public PlanteException(String message) {
+        super(message);
+    }
+}
