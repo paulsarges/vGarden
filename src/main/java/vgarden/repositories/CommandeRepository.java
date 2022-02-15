e8307b5f216f36db4f7cf9273476d80ca68b8cc0
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-02-15 14:26:04 +0100

    Ajout Hibernate Validator

--- src/main/java/vgarden/repositories/CommandeRepository.java
+++ src/main/java/vgarden/repositories/CommandeRepository.java
@@ -13,7 +13,7 @@ public interface CommandeRepository extends JpaRepository<Commande, Long> {
     //@EntityGraph(value = "Commande.CommandeProduits", type = EntityGraph.EntityGraphType.FETCH)
     @Query("SELECT c from Commande c join fetch c.commandeProduits where c.id=:id")
     Optional<Commande> findByIdWithCommandeProduits(@Param("id") Long id);
-    // TODO: Complete query
+
     @Query("SELECT c from Commande c join fetch c.commandeProduits cp where cp.id.produit.plante.terrain.utilisateur = :vendeur")
     List<Commande> findByVendeurWithCommandeProduits(@Param("utilisateur") Utilisateur vendeur);
 }
