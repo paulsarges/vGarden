6f3b661a93d7db48beabe344e1436e017bd28915
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-02-16 10:01:54 +0100

    Modif tests

--- src/test/java/vgarden/CompteSpringTest.java
+++ src/test/java/vgarden/CompteSpringTest.java
@@ -1,12 +1,15 @@
 package vgarden;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import vgarden.model.TypeCompte;
 import vgarden.model.Utilisateur;
 import vgarden.services.CompteServices;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CompteSpringTest {
 
--- src/test/java/vgarden/services/CommandeServiceTest.java
+++ src/test/java/vgarden/services/CommandeServiceTest.java
@@ -1,8 +1,10 @@
 package vgarden.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import vgarden.exceptions.CommandeException;
 import vgarden.model.Commande;
 
@@ -10,6 +12,7 @@ import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CommandeServiceTest {
 
@@ -18,7 +21,7 @@ class CommandeServiceTest {
 
     @Test
     @Transactional
-    public void createMissingUser() {
+    public void createFailMissingUtilisateur() {
         assertThrows(CommandeException.class, () -> {
             Commande commande = new Commande();
             commandeService.createOrUpdate(commande);
