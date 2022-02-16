6c1a31a0d289c4c6bab38c32ff5310601648c886
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-02-16 12:00:47 +0100

    Removed useless entitymanager in commande repository test

--- src/test/java/formation/sopra/vgarden/repositories/CommandeRepositoryTest.java
+++ src/test/java/formation/sopra/vgarden/repositories/CommandeRepositoryTest.java
@@ -6,17 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CommandeRepositoryTest {
-
-    @PersistenceContext
-    private EntityManager em;
 
     @Autowired
     private CommandeRepository commandeRepository;
@@ -35,11 +30,6 @@ class CommandeRepositoryTest {
 
     @Autowired
     private TerrainRepository terrainRepository;
-
-    private void flushAndClear() {
-        em.flush();
-        em.clear();
-    }
 
     @Test
     @Transactional
