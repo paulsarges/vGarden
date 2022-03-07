859a983080c9b8cc6843ac66b0c0df892e2613a8
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-03-07 16:43:28 +0100

    Using authentication pour user info

--- src/main/java/formation/sopra/vgarden/restController/UtilisateurRestController.java
+++ src/main/java/formation/sopra/vgarden/restController/UtilisateurRestController.java
@@ -1,25 +1,18 @@
 package formation.sopra.vgarden.restController;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.annotation.JsonView;
-
 import formation.sopra.vgarden.exceptions.UtilisateurException;
 import formation.sopra.vgarden.model.Compte;
 import formation.sopra.vgarden.model.Utilisateur;
 import formation.sopra.vgarden.model.Views;
 import formation.sopra.vgarden.services.CompteServices;
 import formation.sopra.vgarden.services.UtilisateurServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/utilisateur")
@@ -28,14 +21,20 @@ public class UtilisateurRestController {
 	UtilisateurServices utilisateurServices;
 	@Autowired
 	CompteServices compteServices;
-	
+
 	@GetMapping("/{login}")
 	@JsonView(Views.Common.class)
 	public Utilisateur getUtilisateur(@PathVariable String login) {
 		Compte c = compteServices.getByLogin(login);
 		return utilisateurServices.getByCompte(c);
 	}
-	
+
+	@GetMapping("/info")
+	@JsonView(Views.Common.class)
+	public Utilisateur getUtilisateur(@AuthenticationPrincipal Compte c) {
+		return c.getUtilisateur();
+	}
+
 	@PutMapping("/{login}")
 	@JsonView(Views.Common.class)
 	public Utilisateur modifier(@PathVariable String login,@Valid @RequestBody Utilisateur u, BindingResult br) {
