6dce4e98e38ee2a6b90f1a48a68a7a71e1932474
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-03-09 17:54:21 +0100

    Modif commande

--- src/main/java/formation/sopra/vgarden/model/Commande.java
+++ src/main/java/formation/sopra/vgarden/model/Commande.java
@@ -3,7 +3,6 @@ package formation.sopra.vgarden.model;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +20,6 @@ public class Commande {
 	private Long id;
 
 	@ManyToOne
-	@NotNull
 	@JoinColumn(name = "commande_utilisateur_id", foreignKey = @ForeignKey(name = "commande_utilisateur_id_fk"))
 	@JsonView(Views.CommandeWithUtilisateur.class)
 	private Utilisateur utilisateur;
