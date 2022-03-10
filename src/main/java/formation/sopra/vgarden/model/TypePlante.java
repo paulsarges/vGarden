72d906b92f364bdad9d7afc52cf955cb5e20e067
Fitiantsoa <afitiantsoa@gmail.com>
2022-03-11 00:23:06 +0100

    typeplante

--- src/main/java/formation/sopra/vgarden/model/TypePlante.java
+++ src/main/java/formation/sopra/vgarden/model/TypePlante.java
@@ -1,4 +1,14 @@
 package formation.sopra.vgarden.model;
 
 public enum TypePlante {
-	Ail, Aubergine, Betterave, Carotte, Concombre, Céléri, Chou, Courge, Courgette, Fenouil, Fraisier, Haricot, Laitue, Melon, Poireau, Pois, Poivron, Radis, Tomate}
+	Ail("Ail"), Aubergine("Aubergine"), Betterave("Betterave"), Carotte("Carotte"), Concombre("Concombre"),
+	Céléri("Céléri"), Chou("Chou"), Courge("Courge"), Courgette("Cougette"), Fenouil, Fraisier, Haricot, Laitue, Melon, Poireau, Pois, Poivron, Radis, Tomate}
+	private String nom;
+	
+	private TypePlante(String nom) {
+		this.nom = nom;
+	}
+	public String getNom() {
+		return this.nom;
+	}
+	}
