d34501593c6bb38addcc837b5da30e61fee5e935
pauls <sargespaul@gmail.com>
2022-03-11 00:22:22 +0100

    Ajout nom a enum typeplante

--- src/main/java/formation/sopra/vgarden/model/TypePlante.java
+++ src/main/java/formation/sopra/vgarden/model/TypePlante.java
@@ -1,4 +1,4 @@
 package formation.sopra.vgarden.model;
 
 public enum TypePlante {
-	Ail, Aubergine, Betterave, Carotte, Concombre, Céléri, Chou, Courge, Courgette, Fenouil, Fraisier, Haricot, Laitue, Melon, Poireau, Pois, Poivron, Radis, Tomate}
+	Ail("Ail"), Aubergine("Aubergine"), Betterave("Betterave"), Carotte("Carotte"), Concombre("Concombre"), Céléri("Céléri"), Chou("Chou"), Courge("Courge"), Courgette("Courgette"), Fenouil("Fenouil"), Fraisier, Haricot, Laitue, Melon, Poireau, Pois, Poivron, Radis, Tomate}
