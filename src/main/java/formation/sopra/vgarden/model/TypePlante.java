0f938ede36c803f8c0e8d0a547ff151509a3aa69
IanCretaud <ian.cretaud@gmail.com>
2022-03-10 09:48:53 +0100

    typePlante actualisation + modif terrain restcontroller

--- src/main/java/formation/sopra/vgarden/model/TypePlante.java
+++ src/main/java/formation/sopra/vgarden/model/TypePlante.java
@@ -1,5 +1,4 @@
 package formation.sopra.vgarden.model;
 
 public enum TypePlante {
-Ail, Asperge, Aubergine, Basilic, Brocoli, Carotte, Celeri, Choux, Choux_Fleurs, Ciboulette, Courgette, Fraisier, Framboisier, Haricot, Laitue, Pois, Poireau, Poivron, Radis, Tomate
-}
+	Ail, Aubergine, Betterave, Carotte, Concombre, Céléri, Chou, Courge, Courgette, Fenouil, Fraisier, Haricot, Laitue, Melon, Poireau, Pois, Poivron, Radis, Tomate}
